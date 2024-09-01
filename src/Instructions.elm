daa318f3ed4b5759a7117933aca66fd67ac9e610
Jake Wood <jzwood14@yahoo.com>
2024-08-31 22:36:49 -0400

    think I got types straightened out

--- src/Instructions.elm
+++ src/Instructions.elm
@@ -37,7 +37,7 @@ type alias Children =
 
 
 type Signature
-    = Signature (List Type) Result
+    = Sig (List Type) Result
 
 
 
@@ -45,11 +45,7 @@ type Signature
 
 
 type AST
-    = Module (List Function)
-
-
-type Function
-    = Function Label Signature Children
+    = Module Children
 
 
 type Instruction
@@ -70,6 +66,7 @@ type Instruction
     | Rsh
     | Lsh
     | Num Int
+    | Fun Variable Signature Children
     | Let Variable
     | Set Variable
     | Get Variable
@@ -93,7 +90,7 @@ type Instruction
 
 
 type UI
-    = UI { instr : Instruction, show : String, docs : Docs }
+    = UI { instr : Instruction, button : String, docs : Docs }
 
 
 
@@ -104,12 +101,51 @@ type UI
 
 instructions : List UI
 instructions =
-    [ UI { instr = Add, show = "+", docs = "addition" } ]
+    [ UI { instr = Add, button = "+", docs = "addition" }
+    , UI { instr = Block "" [] [], button = "block", docs = "breaking to block label jumps out of block" }
+    , UI { instr = Loop "" [] [], button = "loop", docs = "breaking to loop label jumps to top of loop" }
+    , UI { instr = If "" [] [] [], button = "if", docs = "breaking to if label jumps out of if" }
+    , UI { instr = Else [], button = "else", docs = "else branch of if statement" }
+    , UI { instr = Add, button = "+", docs = "addition" }
+    , UI { instr = Sub, button = "sub", docs = "subtraction" }
+    , UI { instr = Mul, button = "mult", docs = "multiplication" }
+    , UI { instr = Div, button = "div", docs = "division" }
+    , UI { instr = Rem, button = "%", docs = "remainder" }
+    , UI { instr = Gt, button = ">", docs = "greater than" }
+    , UI { instr = Gte, button = "≥", docs = "greater than or equal" }
+    , UI { instr = Lt, button = "<", docs = "less than" }
+    , UI { instr = Lte, button = "≤", docs = "less than or equal" }
+    , UI { instr = Eq, button = "=", docs = "equal" }
+    , UI { instr = Neq, button = "≠", docs = "not equal" }
+    , UI { instr = And, button = "and", docs = "bitwise/logical and" }
+    , UI { instr = Or, button = "or", docs = "bitwise/logical or" }
+    , UI { instr = Xor, button = "xor", docs = "bitwise/logical xor" }
+    , UI { instr = Rsh, button = "»", docs = "bitwise right shift" }
+    , UI { instr = Lsh, button = "«", docs = "bitwise left shift" }
+    , UI { instr = Num 0, button = "num", docs = "constant number" }
+    , UI { instr = Fun "" (Sig [] []) [], button = "f(x)", docs = "function definition" }
+    , UI { instr = Let "", button = "let", docs = "local variable declaration" }
+    , UI { instr = Set "", button = "set", docs = "set local variable" }
+    , UI { instr = Get "", button = "get", docs = "get local variable" }
+    , UI { instr = Br "", button = "br", docs = "break to label" }
+    , UI { instr = BrIf "", button = "br_if", docs = "if truthy break to label" }
+    , UI { instr = Return, button = "return", docs = "return function" }
+    , UI { instr = Call "", button = "call", docs = "call function" }
+    , UI { instr = Nop, button = "nop", docs = "no operation" }
+    , UI { instr = Drop, button = "drop", docs = "drop top of stack" }
+    , UI { instr = Malloc, button = "malloc", docs = "allocate n bytes" }
+    , UI { instr = Read1, button = "read1", docs = "read 1 bytee" }
+    , UI { instr = Read2, button = "read2", docs = "read 2 bytes" }
+    , UI { instr = Read4, button = "read4", docs = "read 4 bytes" }
+    , UI { instr = Write1, button = "write1", docs = "write 1 byte" }
+    , UI { instr = Write2, button = "write2", docs = "write 2 bytes" }
+    , UI { instr = Write4, button = "write4", docs = "write 4 bytes" }
+    ]
 
 
 toHtml : List UI -> List (Html Msg)
 toHtml ins =
-    List.map (\(UI ui) -> div [ class "op", draggable "true" ] [ text ui.show ]) ins
+    List.map (\(UI ui) -> div [ class "op", draggable "true" ] [ text ui.button ]) ins
 
 
 
